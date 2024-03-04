4d1c862ff080d5e3b4bd918cc5f765b2af399fd8
Elpelfry <delfry_paulino@ucne.edu.do>
2024-03-04 19:52:26 -0400

    Controllers

---
+++ Parcial2_Ap1_DelfryPaulino.API/Controllers/AccesoriosController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Parcial2_Ap1_DelfryPaulino.API.DAL;
+using Parcial2_Ap1_DelfryPaulino.API.ViewModels;
+using Shared.Models;
+
+namespace Parcial2_Ap1_DelfryPaulino.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccesoriosController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public AccesoriosController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Accesorios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Accesorios>>> GetAccesorios()
+        {
+            return await _context.Accesorios.ToListAsync();
+        }
+
+        // GET: api/Accesorios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Accesorios>> GetAccesorios(int id)
+        {
+            var accesorios = await _context.Accesorios.FindAsync(id);
+
+            if (accesorios == null)
+            {
+                return NotFound();
+            }
+
+            return accesorios;
+        }
+
+        // POST: api/Accesorios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Accesorios>> PostAccesorios(Accesorios accesorios)
+        {
+            _context.Accesorios.Add(accesorios);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAccesorios", new { id = accesorios.AccesorioId }, accesorios);
+        }
+
+        // PUT: api/Accesorios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAccesorios(int id, Accesorios accesorios)
+        {
+            if (id != accesorios.AccesorioId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(accesorios).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AccesoriosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Accesorios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAccesorios(int id)
+        {
+            var accesorios = await _context.Accesorios.FindAsync(id);
+            if (accesorios == null)
+            {
+                return NotFound();
+            }
+
+            _context.Accesorios.Remove(accesorios);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AccesoriosExists(int id)
+        {
+            return _context.Accesorios.Any(e => e.AccesorioId == id);
+        }
+    }
+}
---
+++ Parcial2_Ap1_DelfryPaulino.API/Controllers/VehiculosController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Parcial2_Ap1_DelfryPaulino.API.DAL;
+using Shared.Models;
+
+namespace Parcial2_Ap1_DelfryPaulino.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculosController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public VehiculosController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Vehiculos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vehiculos>>> GetVehiculos()
+        {
+            return await _context.Vehiculos.Include(d => d.VehiculosDetalle).ToListAsync();
+        }
+
+        // GET: api/Vehiculos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vehiculos>> GetVehiculos(int id)
+        {
+            var vehiculos = await _context.Vehiculos.
+                Include(d => d.VehiculosDetalle).FirstOrDefaultAsync(v => v.VehiculoId == id);
+
+            if (vehiculos == null)
+            {
+                return NotFound();
+            }
+
+            return vehiculos;
+        }        
+
+        // POST: api/Vehiculos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Vehiculos>> PostVehiculos(Vehiculos vehiculos)
+        {
+            _context.Vehiculos.Add(vehiculos);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVehiculos", new { id = vehiculos.VehiculoId }, vehiculos);
+        }
+
+        // PUT: api/Vehiculos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVehiculos(int id, Vehiculos vehiculos)
+        {
+            if (id != vehiculos.VehiculoId)
+            {
+                return BadRequest();
+            }
+
+
+           
+            await _context.VehiculosDetalle.Where(d => d.VehiculoId == id ).ExecuteDeleteAsync();
+            foreach (var item in vehiculos.VehiculosDetalle)
+            {
+                _context.VehiculosDetalle.Add(item);
+            }
+            _context.Entry(vehiculos).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VehiculosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Vehiculos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehiculos(int id)
+        {
+            var vehiculos = await _context.Vehiculos.FindAsync(id);
+            if (vehiculos == null)
+            {
+                return NotFound();
+            }
+            await _context.VehiculosDetalle.Where(d => d.VehiculoId == id).ExecuteDeleteAsync();
+            _context.Vehiculos.Remove(vehiculos);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VehiculosExists(int id)
+        {
+            return _context.Vehiculos.Any(e => e.VehiculoId == id);
+        }
+    }
+}
