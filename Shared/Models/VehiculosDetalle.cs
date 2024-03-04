1fc158d859477a158696cf6b9e0c8c3dcc073167
Elpelfry <delfry_paulino@ucne.edu.do>
2024-03-04 18:54:42 -0400

    Modelo Detalle

---
+++ Shared/Models/VehiculosDetalle.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Models;
+
+public class VehiculosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    [ForeignKey("Vehiculos")]
+    public int VehiculoId { get; set; }
+    [ForeignKey("Accesorios")]
+    public int AccesorioId { get; set; }
+    [Required(ErrorMessage = "Valor es requerido")]
+    [Range(0.01, 1000000000, ErrorMessage = "El Valor debe estar 0.01 y 1000000000")]
+    public double Valor { get; set; }
+}
