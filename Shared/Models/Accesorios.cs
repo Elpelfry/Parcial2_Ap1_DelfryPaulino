0ac8583d0b33a86b6f26e8b9a410f830a9345218
Elpelfry <delfry_paulino@ucne.edu.do>
2024-03-04 18:54:54 -0400

    Modelo Accesorios

---
+++ Shared/Models/Accesorios.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models;
+
+public class Accesorios
+{
+    [Key]
+    public int AccesorioId { get; set; }
+    [Required(ErrorMessage = "Descripción es obligatorio")]
+    public string? Descripción { get; set; }
+}
