22c36f0598171113ee88886db7716c7fb35a3d7f
Elpelfry <delfry_paulino@ucne.edu.do>
2024-03-04 19:09:30 -0400

    Request Models

---
+++ Parcial2_Ap1_DelfryPaulino.API/ViewModels/AccesoriosRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_Ap1_DelfryPaulino.API.ViewModels;
+
+public class AccesoriosRequest
+{
+    [Required(ErrorMessage = "Descripción es obligatorio")]
+    public string? Descripción { get; set; }
+}
---
+++ Parcial2_Ap1_DelfryPaulino.API/ViewModels/VehiculosRequest.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Parcial2_Ap1_DelfryPaulino.API.ViewModels;
+
+public class VehiculosRequest
+{
+    [Required(ErrorMessage = "Fecha es obligatorio")]
+    public DateTime Fecha { get; set; }
+    [Required(ErrorMessage = "Descripción es obligatorio")]
+    public string? Descripción { get; set; }
+    [Required(ErrorMessage = "Costo es requerido")]
+    [Range(0.01, 1000000000, ErrorMessage = "El costo debe estar 0.01 y 1000000000")]
+    public double Costo { get; set; }
+    public double Gastos { get; set; }
+
+    [ForeignKey("VehiculoId")]
+    public ICollection<VehiculosDetalle> VehiculosDetalle { get; set; } = new List<VehiculosDetalle>();
+}
